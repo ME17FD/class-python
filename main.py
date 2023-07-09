b04b465fba440043c0b88f3ab6bf61790b979725
MEFD <faddak.mehdi@gmail.com>
2023-07-09 07:04:46 +0200

    logging module

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
+import logging
 import sqlite3
 from utilties.structures import *
+from utilties.logging import *
 conn = sqlite3.connect("db\\StudentsDatabase.db")
 c = conn.cursor()
 from utilties.data_filler import *
 studentlst = []
-create_tables(c)
-fill_random_data(c,conn,studentlst,300,0)
 
-#database2py(c,studentlst)
 
-#for i in studentlst:
-#    print(i)
+
+
+if os.path.isfile("db\\StudentsDatabase.db"):
+    database2py(c,studentlst)
+    log("loading database")
+
+
+logging.debug("yo there")
+
---
+++ utilties/logging.py
@@ -0,0 +1,15 @@
+import logging
+import datetime as dt
+import os
+
+def setup():
+    logging.basicConfig(filename="log.txt",level=logging.DEBUG)
+    logging.debug(f'****PROGRAM STARTED : {dt.datetime.now()::%d-%m-%Y %H:%M:%S}****')
+
+
+def log(msg:str):
+        logging.debug(f'{msg} {dt.datetime.now()::%d-%m-%Y %H:%M:%S}****')
+
+def remove_logs():
+    os.remove("log.txt")
+
